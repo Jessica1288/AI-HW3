8b3a57b8f7123703dfaaa7ac63a78ddcd74c319d
Jaewon Shin <jzs0167@auburn.edu>
2018-12-05 18:51:07 -0600

    assignment3
    12/5/2018

---
+++ cartpole_Q.py
@@ -0,0 +1,164 @@
+import random
+import math
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+
+env = gym.make('CartPole-v1')
+env = env.unwrapped
+env.seed(0) #setting environment seed to reproducde same result
+np.random.seed(0) #setting numpy number generation seed to reproduce same random numbers
+
+# outdir = sys.argv[2]
+
+initial_epsilon = 0.1 # probability of choosing a random action (changed from original value of 0.0)
+alpha = 0.05 # learning rate
+lambda_ = 0.9 # trace decay rate
+gamma = 0.99 # discount rate
+N = 30000 # memory for storing parameters 
+episodes = 20
+# M = env.action_space.n
+NUM_TILINGS = 10
+NUM_TILES = 8
+episodescores = []
+iterations = np.zeros([episodes, episodes], int)
+average = np.zeros(episodes, float)
+std_deviation = np.zeros(episodes, float)
+
+def main():
+    # env.monitor.start(outdir)
+
+    epsilon = initial_epsilon
+    theta = np.zeros(N) # parameters (memory)
+
+    for episode_num in range(episodes):
+        for i_episode_num in range(episodes):
+            episodescore, iteration = episode(episode_num, i_episode_num, epsilon, theta, env.spec.timestep_limit)
+            epsilon = epsilon * 0.999 # added epsilon decay
+            episodescores.append(episodescore)
+        std_deviation = np.std(iteration, ddof = 1)
+
+    x = range(episodes)   
+    plt.title('CartPole-v1: Q_learning')
+    # plt.plot(episodescores)
+    # plt.xlabel('Eplside')
+    # plt.ylabel('Reward of Episode')
+    plt.xticks(np.arange(0, 21, 1.0))
+    plt.errorbar(x, average, yerr=std_deviation, fmt='-o')
+    plt.figtext(0.5, 0.01, "Mean value is {} and mean of reward is {}".format(np.mean(average), np.mean(episodescores)), fontsize = 8, va = "bottom", ha = "center")
+    plt.show()
+    # env.monitor.close()
+
+def episode(episode_num, i_episode_num, epsilon, theta, max_steps):
+    Q = np.zeros(env.action_space.n) # action values
+    e = np.zeros(N) # eligibility traces
+    F = np.zeros((env.action_space.n, NUM_TILINGS), dtype=np.int32) # features for each action
+    total_reward = 0
+    def load_F(observation):
+        state_vars = []
+        for i, var in enumerate(observation):
+            range_ = (env.observation_space.high[i] - env.observation_space.low[i])
+            # in CartPole, there is no range on the velocities, so default to 1
+            if range_ == float('inf'):
+                range_ = 1
+            state_vars.append(var / range_ * NUM_TILES)
+
+        for a in range(env.action_space.n):
+            F[a] = get_tiles(NUM_TILINGS, state_vars, N, a)
+
+    def load_Q():
+        for a in range(env.action_space.n):
+            Q[a] = 0
+            for j in range(NUM_TILINGS):
+                Q[a] += theta[F[a,j]]
+
+    observation = env.reset()
+    load_F(observation)
+    load_Q()
+    action = np.argmax(Q) # numpy argmax chooses first in a tie, not random like original implementation
+    if np.random.random() < epsilon:
+        action = env.action_space.sample()
+    # else :
+    #   action = np.argmax()
+    step = 0
+    while True:
+        # env.render()
+        step += 1
+        e *= gamma * lambda_
+        for a in range(env.action_space.n):
+            v = 0.0
+            if a == action:
+                v = 1.0
+
+            for j in range(NUM_TILINGS):
+                e[F[a,j]] = v
+
+        observation, reward, done, info = env.step(action)
+        delta = reward + Q[action]
+        load_F(observation)
+        load_Q()
+        next_action = np.argmax(Q)
+        if np.random.random() < epsilon:
+            next_action = env.action_space.sample()
+        if not done:
+            delta = Q[action] + alpha * (reward + gamma * Q[next_action] - Q[action])
+        # theta += alpha / NUM_TILINGS * delta * e
+        theta += delta
+        load_Q()
+        total_reward += reward
+        if done or step > max_steps:
+            # env.close()
+            break
+        action = next_action
+    iterations[episode_num][i_episode_num] = step + 1
+    average[episode_num] += iterations[episode_num][[i_episode_num]]*1.0 / episodes
+    print("Episode {} completed with total reward {} in {} steps".format(episode_num + 1, total_reward, step))
+    return total_reward, iterations[episode_num]
+
+
+def get_tiles(num_tilings, variables, memory_size, hash_value):
+    num_coordinates = len(variables) + 2
+    coordinates = [0 for i in range(num_coordinates)]
+    coordinates[-1] = hash_value
+
+    qstate = [0 for i in range(len(variables))]
+    base = [0 for i in range(len(variables))]
+    tiles = [0 for i in range(num_tilings)]
+
+    for i, variable in enumerate(variables):
+        qstate[i] = int(math.floor(variable * num_tilings))
+        base[i] = 0
+
+    for j in range(num_tilings):
+        for i in range(len(variables)):
+            if (qstate[i] >= base[i]):
+                coordinates[i] = qstate[i] - ((qstate[i] - base[i]) % num_tilings)
+            else:
+                coordinates[i] = qstate[i] + 1 + ((base[i] - qstate[i] - 1) % num_tilings) - num_tilings
+
+            base[i] += 1 + (2 * i)
+        coordinates[len(variables)] = j
+        tiles[j] = hash_coordinates(coordinates, memory_size)
+
+    return tiles
+
+rndseq = np.random.randint(0, 2**32-1, 2048)
+
+def hash_coordinates(coordinates, memory_size):
+    total = 0
+    for i, coordinate in enumerate(coordinates):
+        index = coordinate
+        index += (449 * i)
+        index %= 2048
+        while index < 0:
+            index += 2048
+
+        total += rndseq[index]
+
+    index = total % memory_size
+    while index < 0:
+        index += memory_size
+
+    return index
+
+main()
---
+++ mountaincar_Q.py
@@ -0,0 +1,111 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+#exploring Mountain Car environment
+
+env = gym.make('MountainCar-v0')
+# assign the hyperparameters
+
+n_states = 40 # number of states
+episodes = 20 # number of episodes/runs
+initial_lr = 1.0 # number of learning rate
+min_lr = 0.005 # minimum learning rate
+gamma = 0.99 # discount factor
+max_steps = 300
+epsilon = 0.05
+episodescores = []
+
+iterations = np.zeros([episodes, episodes], int)
+average = np.zeros(episodes, float)
+std_deviation = np.zeros(episodes, float)
+
+env = env.unwrapped
+env.seed(0) #setting environment seed to reproducde same result
+np.random.seed(0) #setting numpy number generation seed to reproduce same random numbers
+
+
+# perform discretization of the continuous state
+# descretization is the conversion of continuous states space observation
+
+def discretization (env, obs):
+	env_low = env.observation_space.low;
+	env_high = env.observation_space.high;
+
+	env_den = (env_high - env_low) / n_states
+	pos_den = env_den[0]
+	vel_den = env_den[1]
+
+	pos_high = env_high[0]
+	pos_low  = env_low[0]
+	vel_high = env_high[1]
+	vel_low  = env_low[1]
+
+	pos_scaled = int((obs[0] - pos_low)/pos_den)
+	vel_scaled = int((obs[1] - vel_low)/vel_den)
+
+	return pos_scaled, vel_scaled
+
+# Q-learning algorithm by initializing a Q-table/ updating the Q-values
+
+# Q-table
+# rows are states but her state is 2-D pos, vel
+# columns are actions 
+# Q-table would be 3-D
+
+q_table = np.zeros((n_states, n_states, env.action_space.n))
+total_steps = 0
+
+
+for episode in range(episodes):
+	for i_episode in range(episodes):
+		obs = env.reset()
+		total_reward = 0
+		#decreasing learning rate alpha over time
+		alpha = max(min_lr, initial_lr*(gamma **(episode//100)))
+		steps = 0
+		while True:
+			# env.render()
+			pos,vel = discretization(env, obs)
+
+			#action for the current state using epsilon greedy
+			if np.random.uniform(low = 0, high = 1) < epsilon :
+				act = np.random.choice(env.action_space.n)
+			else :
+				# add more information
+				act = np.argmax(q_table[pos][vel])
+
+			obs, reward, done, _ = env.step(act)
+			# total_reward += abs(obs[0] + 0.5)
+			total_reward += reward
+
+			# q_table update
+			pos_,vel_ = discretization(env, obs)
+			# q_table[pos][vel][a] = (1-alpha) * q_table[pos][vel][a] + alpha*(reward + gamma*np.max(q_table[pos_][vel_]))
+			q_table[pos][vel][act] = q_table[pos][vel][act] + alpha *(reward + gamma*np.max(q_table[pos_][vel_]) - q_table[pos][vel][act])
+			steps += 1
+			if done :
+				# env.close()
+				break
+		episodescores.append(total_reward)
+		iterations[episode][i_episode] = steps + 1
+		average[episode] += iterations[episode][[i_episode]] *1.0 / episodes
+	std_deviation = np.std(iterations[episode], ddof = 1)
+	print("Episode {} completed with total reward {} in {} steps".format(episode+1, total_reward, steps))
+
+# print("Average ", np.mean(episodescores))
+
+# while True:  #to hold the render at the last step when Car passes the flag
+# 	env.render()
+# 	plot_running_avg(total_reward)
+plt.title('MountainCar: Q-learning')
+x = range(episodes)   
+plt.xticks(np.arange(0, 21, 1.0))
+plt.errorbar(x, average, yerr=std_deviation, fmt='-o')
+plt.figtext(0.5, 0.01, "Mean value is {} and mean of reward is {}".format(np.mean(average), np.mean(episodescores)), fontsize = 8, va = "bottom", ha = "center")
+
+plt.show()
+
+
+
+
+
